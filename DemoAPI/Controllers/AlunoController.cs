c6160674d8eaeb7fd325bdee11cd09eb901fc811
annarodrigues2 <anna.rodrigues@minervafoods.com>
2025-05-22 20:06:15 -0300

    Adicionar arquivos de projeto.

---
+++ DemoAPI/Controllers/AlunoController.cs
@@ -0,0 +1,27 @@
+﻿using DemoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : ControllerBase
+    {
+       
+
+        [HttpGet("alunos")]
+        public Aluno GetAlunos()
+        {
+
+            return new Aluno
+            {
+                Nome = "Anna Maria",
+                Ativo = true,
+                DataCadastro = DateTime.Now,
+                Email = "teste@gmail.com",
+                Id = 1
+            };
+        }
+
+    }
+}
